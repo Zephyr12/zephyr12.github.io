020fdeda010ad69bf00369bf50e3f61b2f34b63f
willg-c <willg-c@users.noreply.github.com>
2016-11-04 16:28:48 +0000

    test

--- 2.php
+++ 2.php
@@ -1,4 +1,15 @@
 <?php
-phpinfo();
+$page = $_GET['page'];
+
+$people = array("index.html", "scalculus", "blockchain", "atrading","ptheory");
+
+if (in_array($page, $people))
+  {
+  include($page);
+  }
+else
+  {
+  echo "Page not found";
+  }
 
 ?>
